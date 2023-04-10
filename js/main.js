0dd847e3d9625fac93615a11959c44028e1caae8
Nimrod Castano <nimrodcastano@gmail.com>
2023-04-10 23:37:10 +0000

    added event listener to form element

--- js/main.js
+++ js/main.js
@@ -4,3 +4,25 @@ const $img = document.querySelector('img');
 $photoUrl.addEventListener('input', e => {
   $img.setAttribute('src', e.target.value);
 });
+
+const $form = document.querySelector('#entry-form');
+
+// console.log($form.elements);
+
+$form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const inputObj = {
+    title: $form.elements.title.value,
+    photoUrl: $form.elements[1].value,
+    notes: $form.elements.notes.value,
+    entryId: data.nextEntryId
+  };
+
+  data.nextEntryId++;
+  data.entries.unshift(inputObj);
+
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+
+  $form.reset();
+});
