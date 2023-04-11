2354d903b1e121f3a279258be15ab933b823f15b
Nimrod Castano <nimrodcastano@gmail.com>
2023-04-11 20:45:57 +0000

    added functions for dom creation

--- js/main.js
+++ js/main.js
@@ -24,3 +24,47 @@ $form.addEventListener('submit', e => {
 
   $form.reset();
 });
+
+const renderEntry = entry => {
+  const $li = document.createElement('li');
+
+  const $div1 = document.createElement('div');
+  $div1.setAttribute('class', 'row');
+
+  $li.appendChild($div1);
+
+  const $div2a = document.createElement('div');
+  $div2a.setAttribute('class', 'column-half');
+
+  $div1.appendChild($div2a);
+
+  const $img = document.createElement('img');
+  $img.setAttribute('src', entry.photoUrl);
+
+  $div2a.appendChild($img);
+
+  const $div2b = document.createElement('div');
+  $div2b.setAttribute('class', 'column-half');
+
+  $div1.appendChild($div2b);
+
+  const $h2 = document.createElement('h2');
+  $h2.textContent = entry.title;
+
+  const $p = document.createElement('p');
+  $p.textContent = entry.notes;
+
+  $div2b.appendChild($h2);
+  $div2b.appendChild($p);
+
+  return $li;
+};
+
+const $ul = document.querySelector('ul');
+
+document.addEventListener('DOMContentLoaded', e => {
+  for (let i = 0; i < data.entries.length; i++) {
+    const $child = renderEntry(data.entries[i]);
+    $ul.appendChild($child);
+  }
+});
