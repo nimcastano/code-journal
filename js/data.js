2f65c473b5908921a826b2e93ebeeeb69b45078a
Nimrod Castano <nimrodcastano@gmail.com>
2023-04-11 16:45:50 +0000

    added conditional statement to parse and tested feature

--- js/data.js
+++ js/data.js
@@ -11,3 +11,9 @@ window.addEventListener('beforeunload', e => {
   const dataJSON = JSON.stringify(data);
   localStorage.setItem('javascript-local-storage', dataJSON);
 });
+
+const $previousDataJSON = localStorage.getItem('javascript-local-storage');
+
+if ($previousDataJSON !== null) {
+  data = JSON.parse($previousDataJSON);
+}
